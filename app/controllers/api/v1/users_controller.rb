63512e90c65b844e650f2d441bd65b009bbfa68c
tfuquirk <timothyfquirk@gmail.com>
2019-02-12 11:08:58 -0500

    show route added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,10 @@ class Api::V1::UsersController < ApplicationController
 
     render json: @users, status: :ok
   end
+
+  def show
+    @user = User.find(params[:id])
+
+    render json: @user, status: :ok
+  end
 end
