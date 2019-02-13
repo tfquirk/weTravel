1aea0356cac5b9de0b1c33ac7d7148330e7782c2
tfuquirk <timothyfquirk@gmail.com>
2019-02-13 17:37:00 -0500

    create route and booking_params added

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -5,4 +5,15 @@ class Api::V1::BookingsController < ApplicationController
 
     render json: @bookings, status: :ok
   end
+
+  def create
+    Booking.create(booking_params)
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :activity_id, :price, :tickets_reserved, :start_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
    namespace :v1 do
-     resources :users, only: [:index, :show, :update]
+     resources :users, only: [:index, :show]
      resources :countries, only: [:index]
      resources :activities, only: [:index]
-     resources :bookings, only: [:index]
+     resources :bookings, only: [:index, :create]
     end
   end
 
