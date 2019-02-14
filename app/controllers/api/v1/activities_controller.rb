cc95939f7a20c13c4c70467bfd2510b12bf4f6c8
tfuquirk <timothyfquirk@gmail.com>
2019-02-14 13:35:22 -0500

    updated routes to allow for a update/patch to activities

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -5,4 +5,21 @@ class Api::V1::ActivitiesController < ApplicationController
 
     render json: @activities, status: :ok
   end
+
+  def show
+    @activity = Activity.find(params[:id])
+
+    render json: @activity, status: :ok
+  end
+
+  def update
+    @activity = Activity.find(params[:id])
+    @activity.update(activity_params)
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:positions_open)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
    namespace :v1 do
      resources :users, only: [:index, :show]
      resources :countries, only: [:index]
-     resources :activities, only: [:index]
+     resources :activities, only: [:index, :update]
      resources :bookings, only: [:index, :create]
     end
   end
